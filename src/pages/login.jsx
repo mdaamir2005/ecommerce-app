8c5c833760b84404600d767a7812ac4da8b905db
mdaamir2005 <skullcrusher0641@gmail.com>
2025-01-05 23:30:42 +0500

    login page react

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -17,10 +17,11 @@ const navigate = useNavigate();
      
     .string()
     .required('Please Enter your password')
-    .matches(
-      "^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$",
-      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
-    ),
+    .min(8, 'Password must be 8 characters long')
+    .matches(/[0-9]/, 'Password requires a number')
+    .matches(/[a-z]/, 'Password requires a lowercase letter')
+    .matches(/[A-Z]/, 'Password requires an uppercase letter')
+    .matches(/[^\w]/, 'Password requires a symbol'),
   });
 
   const loginFormik = useFormik({
